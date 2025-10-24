0be22393057c152d441142c2b9262a8caeaab0dc
Vini Pasquini <64695549+Vini-Pasquini@users.noreply.github.com>
2025-10-24 02:57:15 -0300

    input system

--- Assets/Code/InputHandler.cs
+++ Assets/Code/InputHandler.cs
@@ -9,31 +9,74 @@ public class InputHandler : PersistentSingleton<InputHandler>
     
     /* Names */
     // Action Maps
-    readonly string USERINTERFACE_ACTIONMAP_NAME = "UI";
-    readonly string PLAYER_ACTIONMAP_NAME = "Player";
+    readonly string _USERINTERFACE_ACTIONMAP_NAME_ = "UI";
+    readonly string _PLAYER_ACTIONMAP_NAME_ = "Player";
     // Actions
-    readonly string PLAYER_MOVEACTION_NAME = "Move";
-    readonly string PLAYER_SHOOTACTION_NAME = "Shoot";
-    readonly string PLAYER_SPECIALACTION_NAME = "Special";
+    readonly string _USERINTERFACE_PAUSEACTION_NAME_ = "Pause";
+    readonly string _PLAYER_MOVEACTION_NAME_ = "Move";
+    readonly string _PLAYER_SHOOTACTION_NAME_ = "Shoot";
+    readonly string _PLAYER_SPECIALACTION_NAME_ = "Special";
 
     /* Action Map References */
     private InputActionMap _userInterfaceActionMap;
     private InputActionMap _playerActionMap;
-    
+
     /* Input Actions */
     // UI
+    private InputAction _uiPauseAction;
     // Player
     private InputAction _playerMoveAction;
     private InputAction _playerShootAction;
     private InputAction _playerSpecialAction;
 
-    private void Start()
-    {
-        this._userInterfaceActionMap = this._inputActionAsset.FindActionMap(USERINTERFACE_ACTIONMAP_NAME);
-        this._playerActionMap = this._inputActionAsset.FindActionMap(PLAYER_ACTIONMAP_NAME);
+    /* Input Values */
 
-        this._playerMoveAction = this._playerActionMap.FindAction(PLAYER_MOVEACTION_NAME);
-        this._playerShootAction = this._playerActionMap.FindAction(PLAYER_SHOOTACTION_NAME);
-        this._playerSpecialAction = this._playerActionMap.FindAction(PLAYER_SPECIALACTION_NAME);
+    private Vector2 _playerMoveInput;
+    public Vector2 PlayerMoveInput { get { return this._playerMoveInput; } }
+
+    private bool _playerShootInput;
+    public bool PlayerShootInput { get { return this._playerShootInput; } }
+
+    private void OnEnable()
+    {
+        this._uiPauseAction.Enable();
+        this._playerMoveAction.Enable();
+        this._playerShootAction.Enable();
+        this._playerSpecialAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        this._uiPauseAction.Disable();
+        this._playerMoveAction.Disable();
+        this._playerShootAction.Disable();
+        this._playerSpecialAction.Disable();
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        // Input Maps
+        this._userInterfaceActionMap = this._inputActionAsset.FindActionMap(_USERINTERFACE_ACTIONMAP_NAME_);
+        this._playerActionMap = this._inputActionAsset.FindActionMap(_PLAYER_ACTIONMAP_NAME_);
+
+        // Input Actions
+        this._uiPauseAction = this._userInterfaceActionMap.FindAction(_USERINTERFACE_PAUSEACTION_NAME_);
+
+        this._playerMoveAction = this._playerActionMap.FindAction(_PLAYER_MOVEACTION_NAME_);
+        this._playerShootAction = this._playerActionMap.FindAction(_PLAYER_SHOOTACTION_NAME_);
+        this._playerSpecialAction = this._playerActionMap.FindAction(_PLAYER_SPECIALACTION_NAME_);
+
+        this.RegisterInputActions();
+    }
+
+    private void RegisterInputActions()
+    {
+        this._playerMoveAction.performed += context => this._playerMoveInput = context.ReadValue<Vector2>();
+        this._playerMoveAction.canceled += context => this._playerMoveInput = Vector2.zero;
+
+        this._playerShootAction.performed += context => this._playerShootInput = true;
+        this._playerShootAction.canceled += context => this._playerShootInput = false;
     }
 }
--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -12,17 +12,15 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        float movementInput = Input.GetAxis(_HORIZONTAL_INPUT_AXIS_); // TODO: change to input system
-
         this._shootTimer = Mathf.Clamp(this._shootTimer - Time.deltaTime, 0, this._shootDelay);
-        if (this._shootTimer <= 0f && Input.GetKey(KeyCode.Space))
+        if (this._shootTimer <= 0f && InputHandler.Instance.PlayerShootInput)
         {
             GameObject newBullet = GameObject.Instantiate(this._bulletPrefab, this.transform.position + (Vector3.up * .5f), Quaternion.identity); // TODO: change to object pooling
             this._shootTimer = this._shootDelay;
         }
 
         Vector3 positionBuffer = this.transform.position;
-        positionBuffer.x = Mathf.Clamp(positionBuffer.x + movementInput * Time.deltaTime * this._movementSpeed, -8f, 8f);
+        positionBuffer.x = Mathf.Clamp(positionBuffer.x + InputHandler.Instance.PlayerMoveInput.x * Time.deltaTime * this._movementSpeed, -8f, 8f);
         this.transform.position = positionBuffer;
     }
 }
